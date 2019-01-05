8aa36818b03d55233914ef2e3221835b39c199e3
Razvan Cosmeanu <razvan.cosmeanu@hootsuite.com>
2019-01-05 19:15:14 +0200

    [FIX] entered too many levels in loop. modify readme.md

--- README.md
+++ README.md
@@ -1,3 +1,243 @@
-\*\*\*\* CONFIGURATOR SHAWORMA
+# CONFIGURATOR SHAWORMA
 
-\*\* Tema pentru Anul I ID, student: Cosmeanu Razvan
+### Tema pentru Anul I ID, student: Cosmeanu Razvan
+
+#### Specificatii
+
+Configuratorul de shaworma este un program care simuleaza interactiunea cu un vanzator de shaworma. Utilizatorul poate:
+
+1. Sa vada un meniu al localului care contine 2 shaworme preconfigurate si optiunea de a crea propria shaworma
+2. Sa isi poata configura propria shaworma (pana la 10 ingrediente) intr-un meniu
+3. Sa adauge shaworma in cosul de cumparaturi (pana la 5 iteme)
+4. Sa ceara bonul de casa cu rezumatul comenzii si pretul total
+
+Programul a fost testat cu compilatorul gcc de pe `macOS High Sierra v10.13.6 (17G4015)`.
+
+Versiune gcc:
+
+```
+Configured with: --prefix=/Library/Developer/CommandLineTools/usr --with-gxx-include-dir=/usr/include/c++/4.2.1
+Apple LLVM version 10.0.0 (clang-1000.10.44.4)
+Target: x86_64-apple-darwin17.7.0
+Thread model: posix
+InstalledDir: /Library/Developer/CommandLineTools/usr/bin
+```
+
+#### Rularea codului
+
+1. `$ git clone https://github.com/RazvanCosmeanu/configurator_shaworma`
+2. `$ cd configurator_shaworma`
+3. `$ gcc shaworma.c`
+4. `$ ./a.out`
+
+#### Interactiunea cu programul
+
+Ruland programul, utilizatorul este intampinat cu o urare si meniul localui
+
+```
+$ ./a.out
+
+Bine ati venit la Shaworma Boierului!
+
+=== MENIU ===
+[0] Nimic, doar ma uitam
+[1] Clasica (berbecut, cu de toate)
+[2] De domnisoare (pui, fara ceapa, maioneza simpla)
+[3] Cum vrei tu
+
+Ce sa fie, sefu?
+```
+
+Utilizatorul poate alege o optiune introducand numarul de langa fiecare alegere si apasand tasta `Enter`:
+
+```
+...
+Ce sa fie, sefu?1
+
+S-a pus o clasica.
+
+=== MENIU ===
+[0] Nimic, doar ma uitam
+[1] Clasica (berbecut, cu de toate)
+[2] De domnisoare (pui, fara ceapa, maioneza simpla)
+[3] Cum vrei tu
+[4] Atat sa fie, nota va rog
+
+Ce sa fie, sefu?2
+
+S-a pus una de domnisoare.
+
+=== MENIU ===
+[0] Nimic, doar ma uitam
+[1] Clasica (berbecut, cu de toate)
+[2] De domnisoare (pui, fara ceapa, maioneza simpla)
+[3] Cum vrei tu
+[4] Atat sa fie, nota va rog
+
+Ce sa fie, sefu?
+```
+
+Utilizatorul isi poate personaliza shaworma cu ajutorul unei liste de ingrediente:
+
+```
+...
+Ce sa fie, sefu?3
+
+=== MENIU INGREDIENTE ===
+[0] porc
+[1] pui
+[2] vita
+[3] berbecut
+[4] maioneza simpla
+[5] maioneza cu usturoi
+[6] ketchup dulce
+[7] ketchup picant
+[8] varza alba
+[9] varza rosie
+[10] ceapa
+[11] castraveti simpli
+[12] castraveti murati
+0
+
+Punem porc, avem 1 ingrediente (nu am lucrat la pluraluri)
+
+=== MENIU INGREDIENTE ===
+[0] porc
+[1] pui
+[2] vita
+[3] berbecut
+[4] maioneza simpla
+[5] maioneza cu usturoi
+[6] ketchup dulce
+[7] ketchup picant
+[8] varza alba
+[9] varza rosie
+[10] ceapa
+[11] castraveti simpli
+[12] castraveti murati
+2
+
+Punem vita, avem 2 ingrediente
+
+=== MENIU INGREDIENTE ===
+[0] porc
+[1] pui
+[2] vita
+[3] berbecut
+[4] maioneza simpla
+[5] maioneza cu usturoi
+[6] ketchup dulce
+[7] ketchup picant
+[8] varza alba
+[9] varza rosie
+[10] ceapa
+[11] castraveti simpli
+[12] castraveti murati
+[13] Atat boss
+8
+
+Punem varza alba, avem 3 ingrediente
+
+=== MENIU INGREDIENTE ===
+[0] porc
+[1] pui
+[2] vita
+[3] berbecut
+[4] maioneza simpla
+[5] maioneza cu usturoi
+[6] ketchup dulce
+[7] ketchup picant
+[8] varza alba
+[9] varza rosie
+[10] ceapa
+[11] castraveti simpli
+[12] castraveti murati
+[13] Atat boss
+13
+
+=== MENIU ===
+[0] Nimic, doar ma uitam
+[1] Clasica (berbecut, cu de toate)
+[2] De domnisoare (pui, fara ceapa, maioneza simpla)
+[3] Cum vrei tu
+[4] Atat sa fie, nota va rog
+
+Ce sa fie, sefu?
+```
+
+Utilizatorul poate cere nota de plata:
+
+```
+...
+Ce sa fie, sefu?4
+
+Asta e nota, boss
+
+Avem una bucata cu:
+- berbecut
+- maioneza simpla
+- maioneza cu usturoi
+- ketchup dulce
+- ketchup picant
+- varza alba
+- varza rosie
+- ceapa
+- castraveti simpli
+- castraveti murati
+
+Pret: 20
+
+Avem una bucata cu:
+- pui
+- maioneza simpla
+- ketchup dulce
+- varza alba
+- varza rosie
+- castraveti simpli
+- castraveti murati
+
+Pret: 14
+
+Avem una bucata cu:
+- porc
+- vita
+- varza alba
+
+Pret: 6
+
+
+*** Avem 3 shaworma la pretul de 40 de lei, mergeti cu bonuletul la casa va rog ***
+
+
+Urmatorul, va rog! Hai, ca lumea asteapta!
+
+
+=== MENIU ===
+[0] Nimic, doar ma uitam
+[1] Clasica (berbecut, cu de toate)
+[2] De domnisoare (pui, fara ceapa, maioneza simpla)
+[3] Cum vrei tu
+
+Ce sa fie, sefu?
+```
+
+Dupa generarea notei de plata, un nou utilizator poate fi servit. Un utilizator poate parasi programul prin alegand optiunea `0`:
+
+```
+...
+Ce sa fie, sefu?0
+
+Hai ca tii coada, boss!
+```
+
+#### Lista functiilor
+
+1. `void render_choices()` -> Prezinta meniul si trateaza inputul ales de utilizator
+2. `void render_menu()` -> Printeaza meniul localui
+3. `void render_custom_shaworma_menu` -> Printeaza meniul de configurare al shawormelor si trateaza inputul ales de utilizator
+4. `void add_shaworma_to_basket` -> Adauga shaworma in cos
+5. `void close_order` -> printeaza bonul si reseteaza variabilele necesare unei noi comenzi
+
+#### Ordine executie
+
+Codul si fiecare ramura decizionala din el sunt comentate inline
--- shaworma.c
+++ shaworma.c
@@ -43,7 +43,7 @@ const int ING_COUNT_SHAWORMA_DOMNISOARE = 7;
 
 // VARIABILA IN CARE TINEM NUMARUL INGREDIENTELOR DIN SHAWORMA
 int ingredient_no = 0;
-// VARIABILA IN CARE TINEM MARIMEA COSULUI
+// VARIABILA IN CARE TINEM MARIMEA ACTIVA COSULUI
 int basket_length = 0;
 // SHAWORMA TEMPORARA IN CARE TINEM FIECARE SHAWORMA PERSONALIZATA INAINTE SA O ADAUGAM IN COS
 struct Shaworma temp_shaworma;
@@ -61,131 +61,7 @@ int main()
   return 0;
 }
 
-void render_menu()
-{
-  printf("\n=== MENIU ===\n");
-  printf("[0] Nimic, doar ma uitam\n");
-  printf("[1] Clasica (berbecut, cu de toate)\n");
-  printf("[2] De domnisoare (pui, fara ceapa, maioneza simpla)\n");
-  printf("[3] Cum vrei tu\n");
-  // DACA AVEM CEVA IN COS, PRINTAM OPTIUNEA DE A CERE NOTA
-  if (basket_length > 0)
-  {
-    printf("[4] Atat sa fie, nota va rog\n");
-  }
-}
-
-void add_shaworma_to_basket()
-{
-  // CALCULAM PRETUL SHAWORMEI IN FUNCTIE DE INGREDIENTE SI COSTUL PER INGREDIENT
-  temp_shaworma.price = ingredient_no * COST_INGREDIENT;
-  // ADAUGAM NR DE INGREDIENTE
-  temp_shaworma.ing_size = ingredient_no;
-  // ADAUGAM SHAWORMA IN COS
-  basket[basket_length] = temp_shaworma;
-  // MARIMEA COSULUI CRESTE CU 1
-  basket_length++;
-  // REINITIALIZAM SHAWORMA CU UNA GOALA
-  temp_shaworma = empty_shaworma;
-  // REINITIALIZAM NUMARUL INGREDIENTELOR
-  ingredient_no = 0;
-
-  if (basket_length >= MAX_NUM_COMANDA)
-  {
-    // DACA AVEM DEJA NUMARUL MAXIM DE SHAWORME IN COS, PRINTAM NOTA DE PLATA
-    printf("\nHai ca nu facem comanda pentru tot biroul, da?\n");
-    close_order();
-  }
-  else
-  {
-    // ALTFEL, PRINTAM MENIUL
-    render_choices();
-  }
-}
-
-void close_order()
-{
-  // INITIALIZAM TOTALUL CU 0
-  int total = 0;
-  // TRAVERSAM COSUL
-  for (int i = 0; i < basket_length; i++)
-  {
-    // ADAUGAM PRETUL SHAWORMELOR LA TOTAL
-    total += basket[i].price;
-    // TRAVERSAM INGREDIENTELE FIECAREI SHAWORMA
-    printf("\nAvem una bucata cu: \n");
-    for (int j = 0; j < basket[i].ing_size; j++)
-    {
-      // PRINTAM NUMELE INGREDIENTELOR
-      printf("- %s \n", INGREDIENTE[basket[i].ingredients[j]]);
-    }
-    // PRINTAM PRETUL
-    printf("\nPret: %d\n", basket[i].price);
-  }
-  printf("\n\n*** Avem %d shaworma la pretul de %d de lei, mergeti cu bonuletul la casa va rog *** \n\n", basket_length, total);
-  printf("\nUrmatorul, va rog! Hai, ca lumea asteapta!\n\n");
-
-  // REINITIALIZAM LUNGIMEA COSULUI
-  basket_length = 0;
-  // NU TREBUIE SA GOLIM COSUL, PRODUSELE SE VOR SCRIE DOAR IN ORDINE SI NOTA DE PLATA SE VA FACE IN FUNCTIE DE basket_length
-}
-
-void render_custom_shaworma_menu()
-{
-  char line[100];
-  int answer;
-  int sscanf_result;
-  answer = -1;
-  // CAT TIMP RASPUNSUL NU ESTE 13 - FINALIZAREA CONFIGURARII
-  while (answer != 13)
-  {
-    printf("\n=== MENIU INGREDIENTE ===\n");
-    // trecem prin arrayul ingrediente si printam fiecare optiune
-    for (int i = 0; i < 13; i++)
-    {
-      printf("[%d] %s \n", i, INGREDIENTE[i]);
-    }
-    // daca utilizatorul a adaugat cel putin un ingredient, poate termina shaworma si o poate adauga in cos
-    if (ingredient_no > 1)
-    {
-      printf("[13] Atat boss\n");
-    }
-
-    fgets(line, sizeof(line), stdin);
-    sscanf_result = sscanf(line, "%d", &answer);
-    // DACA RASPUNSUL NU ESTE UNUL VALID, PUNEM ANSWER CA INVALID PENTRU A INTRA IN RAMURA CORECTA DE SWITCH
-    if ((sscanf_result == 0) | (sscanf_result == EOF))
-    {
-      printf("\nD-astea n-avem! Doar ce-i pe meniu, totusi...\n");
-      answer = -1;
-    }
-    switch (answer)
-    {
-    case 13:
-      add_shaworma_to_basket();
-      break;
-    default:
-      // DACA MAI AVEM LOC IN SHAWORMA, PUNEM INGREDIENTUL IN SHAWORMA TEMPORARA SI MARIM NUMARUL ACTIV DE INGREDIENTE
-      if (answer == -1)
-      {
-        break;
-      }
-      printf("\nPunem %s, avem %d ingrediente\n", INGREDIENTE[answer], ingredient_no + 1);
-      // PUNEM INDICELE INGREDIENTULUI IN SHAWORMA TERMPORARA
-      temp_shaworma.ingredients[ingredient_no] = answer;
-      // MARIM NUMARUL DE INGREDIENTE
-      ingredient_no++;
-      // VERIFICAM DACA NUMARUL DE INGREDENTE MAXIM A FOST ATINS
-      if (ingredient_no >= 10)
-      {
-        printf("Am pus maximul de %d ingrediente !\n", MAX_NUM_INGREDIENTS);
-        // NUMARUL MAXIM DE INGREDIENTE A FOST ATINS, ADAUGAM SHAWORMA IN COS
-        add_shaworma_to_basket();
-      }
-      break;
-    }
-  }
-}
+int from_main = 0;
 
 void render_choices()
 {
@@ -244,4 +120,130 @@ void render_choices()
       break;
     }
   }
+}
+
+void render_menu()
+{
+
+  printf("\n=== MENIU ===\n");
+  printf("[0] Nimic, doar ma uitam\n");
+  printf("[1] Clasica (berbecut, cu de toate)\n");
+  printf("[2] De domnisoare (pui, fara ceapa, maioneza simpla)\n");
+  printf("[3] Cum vrei tu\n");
+  // DACA AVEM CEVA IN COS, PRINTAM OPTIUNEA DE A CERE NOTA
+  if (basket_length > 0)
+  {
+    printf("[4] Atat sa fie, nota va rog\n");
+  }
+}
+
+void render_custom_shaworma_menu()
+{
+  char line[100];
+  int answer;
+  int sscanf_result;
+  answer = -1;
+  // CAT TIMP RASPUNSUL NU ESTE 13 - FINALIZAREA CONFIGURARII SAU INAPOI LA MENIU
+  while (answer != 13 && answer != 14)
+  {
+    printf("\n=== MENIU INGREDIENTE ===\n");
+    // TRECEM PRIN ARRAYUL INGREDIENTE SI PRINTAM FIECARE OPTIUNE
+    for (int i = 0; i < 13; i++)
+    {
+      printf("[%d] %s \n", i, INGREDIENTE[i]);
+    }
+    // DACA UTILIZATORUL A ADAUGAT CEL PUTIN DOUA INGREDIENTE, POATE TERMINA SHAWORMA SI O POATE ADAUGA IN COS
+    if (ingredient_no > 1)
+    {
+      printf("[13] Atat boss\n");
+    }
+    printf("[14] Inapoi la meniu\n");
+
+    fgets(line, sizeof(line), stdin);
+    sscanf_result = sscanf(line, "%d", &answer);
+    // DACA RASPUNSUL NU ESTE UNUL VALID, PUNEM ANSWER CA INVALID PENTRU A INTRA IN RAMURA CORECTA DE SWITCH
+    if ((sscanf_result == 0) | (sscanf_result == EOF))
+    {
+      printf("\nD-astea n-avem! Doar ce-i pe meniu, totusi...\n");
+      answer = -1;
+    }
+    switch (answer)
+    {
+    case 14:
+      render_choices();
+      break;
+    case 13:
+      add_shaworma_to_basket();
+      render_choices();
+      break;
+    default:
+      // DACA MAI AVEM LOC IN SHAWORMA, PUNEM INGREDIENTUL IN SHAWORMA TEMPORARA SI MARIM NUMARUL ACTIV DE INGREDIENTE
+      if (answer == -1)
+      {
+        break;
+      }
+      printf("\nPunem %s, avem %d ingrediente\n", INGREDIENTE[answer], ingredient_no + 1);
+      // PUNEM INDICELE INGREDIENTULUI IN SHAWORMA TERMPORARA
+      temp_shaworma.ingredients[ingredient_no] = answer;
+      // MARIM NUMARUL DE INGREDIENTE
+      ingredient_no++;
+      // VERIFICAM DACA NUMARUL DE INGREDENTE MAXIM A FOST ATINS
+      if (ingredient_no >= 10)
+      {
+        printf("Am pus maximul de %d ingrediente !\n", MAX_NUM_INGREDIENTS);
+        // NUMARUL MAXIM DE INGREDIENTE A FOST ATINS, ADAUGAM SHAWORMA IN COS
+        add_shaworma_to_basket();
+      }
+      break;
+    }
+  }
+}
+
+void add_shaworma_to_basket()
+{
+  // CALCULAM PRETUL SHAWORMEI IN FUNCTIE DE INGREDIENTE SI COSTUL PER INGREDIENT
+  temp_shaworma.price = ingredient_no * COST_INGREDIENT;
+  // ADAUGAM NR DE INGREDIENTE
+  temp_shaworma.ing_size = ingredient_no;
+  // ADAUGAM SHAWORMA IN COS
+  basket[basket_length] = temp_shaworma;
+  // MARIMEA COSULUI CRESTE CU 1
+  basket_length++;
+  // REINITIALIZAM SHAWORMA CU UNA GOALA
+  temp_shaworma = empty_shaworma;
+  // REINITIALIZAM NUMARUL INGREDIENTELOR
+  ingredient_no = 0;
+  if (basket_length >= MAX_NUM_COMANDA)
+  {
+    // DACA AVEM DEJA NUMARUL MAXIM DE SHAWORME IN COS, PRINTAM NOTA DE PLATA
+    printf("\nHai ca nu facem comanda pentru tot biroul, da?\n");
+    close_order();
+  }
+}
+
+void close_order()
+{
+  // INITIALIZAM TOTALUL CU 0
+  int total = 0;
+  // TRAVERSAM COSUL
+  for (int i = 0; i < basket_length; i++)
+  {
+    // ADAUGAM PRETUL SHAWORMELOR LA TOTAL
+    total += basket[i].price;
+    // TRAVERSAM INGREDIENTELE FIECAREI SHAWORMA
+    printf("\nAvem una bucata cu: \n");
+    for (int j = 0; j < basket[i].ing_size; j++)
+    {
+      // PRINTAM NUMELE INGREDIENTELOR
+      printf("- %s \n", INGREDIENTE[basket[i].ingredients[j]]);
+    }
+    // PRINTAM PRETUL
+    printf("\nPret: %d\n", basket[i].price);
+  }
+  printf("\n\n*** Avem %d shaworma la pretul de %d de lei, mergeti cu bonuletul la casa va rog *** \n\n", basket_length, total);
+  printf("\nUrmatorul, va rog! Hai, ca lumea asteapta!\n\n");
+
+  // REINITIALIZAM LUNGIMEA COSULUI
+  basket_length = 0;
+  // NU TREBUIE SA GOLIM COSUL, PRODUSELE SE VOR SCRIE DOAR IN ORDINE SI NOTA DE PLATA SE VA FACE IN FUNCTIE DE basket_length
 }
